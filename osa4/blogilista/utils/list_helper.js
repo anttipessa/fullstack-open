2fba6cd0e314a8e4dd02f4b2d76eaf680b72303e
Antti Pessa <anttipessa@gmail.com>
2020-06-07 14:21:00 +0300

    4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likes = blogs.map(x => x.likes)
+    
+    const reducer = (sum, item) => {
+        return sum + item
+      }
+      return likes.length === 0
+        ? 0 
+        : likes.reduce(reducer, 0)
+    
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
