79a71630b1eedb643659ab1f225612ce3d97dcc0
Antti Pessa <anttipessa@gmail.com>
2020-06-14 16:03:38 +0300

    4.17

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,14 +1,28 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 , url: 1})
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  const user = await User.findOne()
+  console.log(user)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
+
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  
   response.json(savedBlog.toJSON())
 })
 
@@ -30,7 +44,7 @@ blogsRouter.put('/:id', async (request, response) => {
   Object.keys(update).forEach(key => {
     if (!update[key]) delete update[key]
   })
-  
+
   await Blog.findByIdAndUpdate(request.params.id, update)
   response.status(204).end()
 })
