44ecd2974eef61959e794103ce87c8c9353320e7
Antti Pessa <anttipessa@gmail.com>
2020-09-07 16:11:33 +0300

    8.2

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const uuid = require('uuid')
 
 let authors = [
   {
@@ -84,16 +85,29 @@ let books = [
 ]
 
 const typeDefs = gql`
+
+type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: (root, args) => {
+        return books
+      }
   }
 }
 
