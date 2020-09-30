ebdbb27a7c4974d5a7d4a5891eca21813dab9e90
Antti Pessa <anttipessa@gmail.com>
2020-09-30 21:27:46 +0300

    8.7

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -115,6 +115,10 @@ type Book {
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }  
 `
 
@@ -140,11 +144,18 @@ const resolvers = {
     addBook: (root, args) => {
       const book = { ...args, id: uuid() }
       books = books.concat(book)
-      if (!authors.find(a => a.name === args.author)){
+      if (!authors.find(a => a.name === args.author)) {
         const author = { name: args.author, born: null, id: uuid() }
         authors = authors.concat(author)
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) return null
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
