b491fccabdd231c2c9fde90f0005991d89293520
Antti Pessa <anttipessa@gmail.com>
2020-10-02 20:44:58 +0300

    8.9

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -4,13 +4,14 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from './queries'
+import { ALL_AUTHORS, ALL_BOOKS } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
-  const result = useQuery(ALL_AUTHORS)
+  const authors = useQuery(ALL_AUTHORS)
+  const books = useQuery(ALL_BOOKS)
 
-  if (result.loading) {
+  if (authors.loading || books.loading) {
     return <div>loading...</div>
   }
 
@@ -24,11 +25,12 @@ const App = () => {
 
       <Authors
         show={page === 'authors'}
-        authors={result.data.allAuthors}
+        authors={authors.data.allAuthors}
       />
 
       <Books
         show={page === 'books'}
+        books={books.data.allBooks}
       />
 
       <NewBook
